57fb2c9f6b2244f3f754fd63bcc49135859edce2
Remi Jannel <remi@stripe.com>
2017-11-07 10:46:44 -0500

    Move CountrySpec, ApplePayDomain and Balance to stripe-mock

--- tests/api_resources/test_balance.py
+++ tests/api_resources/test_balance.py
@@ -1,17 +1,14 @@
 from __future__ import absolute_import, division, print_function
 
 import stripe
-from tests.helper import StripeResourceTest
+from tests.helper import StripeMockTestCase
 
 
-class BalanceTest(StripeResourceTest):
-
-    def test_retrieve_balance(self):
-        stripe.Balance.retrieve()
-
-        self.requestor_mock.request.assert_called_with(
+class BalanceTest(StripeMockTestCase):
+    def test_is_retrievable(self):
+        resource = stripe.Balance.retrieve()
+        self.assert_requested(
             'get',
-            '/v1/balance',
-            {},
-            None
+            '/v1/balance'
         )
+        self.assertIsInstance(resource, stripe.Balance)
--- tests/api_resources/test_country_spec.py
+++ tests/api_resources/test_country_spec.py
@@ -1,25 +1,23 @@
 from __future__ import absolute_import, division, print_function
 
 import stripe
-from tests.helper import StripeResourceTest
+from tests.helper import StripeMockTestCase
 
 
-class CountrySpecTest(StripeResourceTest):
-
-    def test_country_spec_list(self):
-        stripe.CountrySpec.list()
-        self.requestor_mock.request.assert_called_with(
+class CountrySpecTest(StripeMockTestCase):
+    def test_is_listable(self):
+        resources = stripe.CountrySpec.list()
+        self.assert_requested(
             'get',
-            '/v1/country_specs',
-            {}
+            '/v1/country_specs'
         )
+        self.assertIsInstance(resources.data, list)
+        self.assertIsInstance(resources.data[0], stripe.CountrySpec)
 
-    def test_country_spec_retrieve(self):
-        stripe.CountrySpec.retrieve('US')
-
-        self.requestor_mock.request.assert_called_with(
+    def test_is_retrievable(self):
+        resource = stripe.CountrySpec.retrieve('US')
+        self.assert_requested(
             'get',
-            '/v1/country_specs/US',
-            {},
-            None
+            '/v1/country_specs/US'
         )
+        self.assertIsInstance(resource, stripe.CountrySpec)
